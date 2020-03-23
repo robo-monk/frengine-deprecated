215fabe450fdd2138f6c3e45ad29984fea58abb7
robo-monk <filtap007@gmail.com>
2020-03-23 19:52:28 +0200

    clean code

--- app.py
+++ app.py
@@ -6,40 +6,52 @@ import os
 import random
 import string
 
-
-# functions
+# utility functions
 
 def download(url, file_name):
   with open(file_name, "wb") as file:
-      response = requests.get(url)
-      file.write(response.content)
-
-def extract(link):
-  response = requests.get(link)
-  doc = Document(response.text)
-  return doc.title(), doc.summary()
+    response = requests.get(url)
+    file.write(response.content)
 
 def rand_str():
   ''.join([random.choice(string.ascii_letters + string.digits)
            for n in range(32)])
 
+def link2html(link):
+  response = requests.get(link)
+  doc = Document(response.text)
+  return doc.title(), doc.summary()
+
+def pdf2html(url):
+  path_in = f'temp/{rand_str()}.pdf'
+  download(url, path_in)
+  path_out = f'temp/{rand_str()}.html'
+  subprocess.call(f"pdf2htmlEX {path_in} {path_out}", shell=True)
+  with open(path_out, 'r') as file:
+    data = file.read().replace('\n', '')
+    os.remove(path_in)
+    os.remove(path_out)
+  return (data)
+
+
 app = Flask(__name__)
   
 
 @app.route('/')
-def home():
+def ctrl_home():
   return "<h1> Frengine </h1> <p> Find out about this project on <a href='https://github.com/robo-monk/frengine'> GitHub </a> </p>"
 
-
-@app.route("/<path:link>")
-def preview(link):
-  title, body = extract(link)
+# LINK 2 HTML
+@app.route("/link2html/<path:link>")
+def ctrl_link2html(link):
+  title, body = link2html(link)
   return f"<h1>{title}</h1>" + body
 
-@app.route("/api/<path:link>")
-def api(link):
+
+@app.route("/api/link2html/<path:link>")
+def ctrl_link2html_api(link):
   try:
-    title, body = extract(link)
+    title, body = link2html(link)
     data = {
       "title" : title,
       "summary" : body
@@ -47,44 +59,23 @@ def api(link):
     return jsonify(data)
   except:
     return abort(400)
-  
-@app.route("/pdf2html/<path:url>")
-def convert_to_html(url):
-  try:
-    path_in = f'temp/{rand_str()}.pdf'
-    print(path_in)
-    download(url, path_in)
-    path_out = f'temp/{rand_str()}.html'
-    print(path_out)
-    subprocess.call(f"pdf2htmlEX {path_in} {path_out}", shell=True)
-    
-    with open(path_out, 'r') as file:
-      data = file.read().replace('\n', '')
 
-    os.remove(path_in)
-    os.remove(path_out)
-    
-    return (data)
-  except:
-    return abort(400)
+
+# PDF 2 HTML
+@app.route("/pdf2html/<path:url>")
+def ctrl_pdf2html(url):
+  return pdf2html(url)
 
 
 @app.route("/api/pdf2html/<path:url>")
 def convert_to_html_api(url):
   try:
-    path_in = f'temp/{rand_str()}.pdf'
-    download(url, path_in)
-    path_out = f'temp/{rand_str()}.html'
-    subprocess.call(f"pdf2htmlEX {path_in} {path_out}", shell=True)
-    with open(path_out, 'r') as file:
-      data = file.read().replace('\n', '')
-    
-    # os.remove(path_in)
-    os.remove(path_out)
-    return jsonify(data)
+    return jsonify(pdf2html(url))
   except:
     return abort(400)
 
+
+#START APP
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
 
