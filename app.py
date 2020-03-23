e1b2e864645d34ec84ddd7d1b4f8bacb2b0e92b0
robo-monk <filtap007@gmail.com>
2020-03-23 18:30:04 +0200

    first working pdf2html

--- app.py
+++ app.py
@@ -1,18 +1,34 @@
 from flask import Flask, jsonify, abort
 import requests
+import subprocess
 from readability import Document
+import os
+import random
+import string
+
+
+# functions
+
+def download(url, file_name):
+  with open(file_name, "wb") as file:
+      response = requests.get(url)
+      file.write(response.content)
 
 def extract(link):
   response = requests.get(link)
   doc = Document(response.text)
   return doc.title(), doc.summary()
-  
+
+def rand_str():
+  ''.join([random.choice(string.ascii_letters + string.digits)
+           for n in range(32)])
 
 app = Flask(__name__)
+  
 
 @app.route('/')
 def home():
-    return "<h1> Frengine </h1> <p> Find out about this project on <a href='https://github.com/robo-monk/frengine'> GitHub </a> </p>"
+  return "<h1> Frengine </h1> <p> Find out about this project on <a href='https://github.com/robo-monk/frengine'> GitHub </a> </p>"
 
 
 @app.route("/<path:link>")
@@ -33,14 +49,22 @@ def api(link):
     return abort(400)
 
 
+@app.route("/api/pdf_to_html/<path:url>")
+def convert_to_html(url):
+  try:
+    path_in = f'temp/{rand_str()}.pdf'
+    download(url, path_in)
+    path_out = f'temp/{rand_str()}.html'
+    subprocess.call(f"pdf2htmlEX {path_in} {path_out}", shell=True)
+    with open(path_out, 'r') as file:
+      data = file.read().replace('\n', '')
+    
+    # os.remove(path_in)
+    os.remove(path_out)
+    return jsonify(data)
+  except:
+    return abort(400)
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
-# @app.route('/')
-# def home():
-#   return 'README'
-
-
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
